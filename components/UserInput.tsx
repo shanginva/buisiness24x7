1d501f08067ae03819c7048886117a9bbe5ed921
Vladimir Shangin <shangin_vl@hotmail.com>
2022-03-16 08:51:40 +0100

    Refactor to use hooks

--- components/UserInput.tsx
+++ components/UserInput.tsx
@@ -2,6 +2,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { Pressable, StyleSheet, TextInput, View } from 'react-native';
+import AudioRecording from './AudioRecording';
+import * as FileSystem from 'expo-file-system';
+import { useSound } from '../hooks/useSound';
+import { useSpeech } from '../hooks/useSpeech';
 
 export type UserInputProps = {
   onSubmit: (message: string) => void;
@@ -9,14 +13,22 @@ export type UserInputProps = {
 
 const UserInput = ({ onSubmit }: UserInputProps) => {
   const [input, setInput] = React.useState('');
+  const [play] = useSound();
+  const { textToSpeech } = useSpeech();
 
-  const onSave = () => {
+  const onSave = async () => {
     if (input === '') {
       return;
     }
     onSubmit(input);
+    const soundData = await textToSpeech(input);
+    const fileUri = `${FileSystem.documentDirectory}/tmp.mp3`;
+    console.log(fileUri);
+    await FileSystem.writeAsStringAsync(fileUri, soundData, { encoding: FileSystem.EncodingType.Base64 });
+    await play(fileUri);
     setInput('');
   };
+
 
   return (
     <View style={{ ...styles.view }}>
@@ -25,12 +37,13 @@ const UserInput = ({ onSubmit }: UserInputProps) => {
         value={input}
         onChangeText={setInput}
         onSubmitEditing={onSave}
-        placeholder="Type something"
+        placeholder="Задайте вопрос"
       />
-      {!!input &&
-        <Pressable onPress={onSave}>
+      {input
+        ? <Pressable onPress={onSave}>
           <FontAwesomeIcon style={{ marginRight: 15 }} icon={faPaperPlane} />
-        </Pressable>}
+        </Pressable>
+        : <AudioRecording />}
     </View>);
 }
 
@@ -38,7 +51,7 @@ const styles = StyleSheet.create({
   input: {
     height: 40,
     margin: 12,
-    borderWidth: 1,
+    borderWidth: 0,
     padding: 10,
     flex: 1
   },
